382049df77b17953ad9c670f7c320e9ba2a49a39
Pallandor <roger.sejas@gmail.com>
2016-03-12 23:23:12 +1100

    Refactored table rendering functionality

--- blogapp.js
+++ blogapp.js
@@ -1,26 +1,24 @@
 // For purposes of adding a blog post: 
-// var localPostsTotal;
-// var renderTable = function(blog) {
-//     blog.blogPosts.forEach(function(post) {
-//         $('.blog-table-body').append('<tr><th>' + post.number + '</th><td>' + post.date + '</td><td>' + post.content + '</td></tr>');
-//     });
-// };
+var table = {
+    localPostsTotal: null,
+    render: function tableRenderer(blog) {
+    	// Track the locally rendered table's total posts
+    	this.localPostsTotal = blog.totalBlogPosts; 
+
+        blog.blogPosts.forEach(function(post) {
+            $('.blog-table-body').append('<tr><th>' + post.number + '</th><td>' + post.date + '</td><td>' + post.content + '</td></tr>');
+        });
+    }
+};
 
 // $ document.ready
 $(function() {
 
     // Render blog table with current blog posts
     $.get('/database.json', function(data) {
-
-    	console.log('is this get request running?');
-    	
         // If jQuery doesn't automatically return a JS object, parse the JSON string
         var blogData = typeof data === 'string' ? JSON.parse(data) : data;
-        // localPostsTotal = blogData.totalBlogPosts;
-
-        blogData.blogPosts.forEach(function(post) {
-            $('.blog-table-body').append('<tr><th>' + post.number + '</th><td>' + post.date + '</td><td>' + post.content + '</td></tr>');
-        });
+        table.render(blogData);
     });
 
     // Add blog post
@@ -31,6 +29,8 @@ $(function() {
     // if localPostsTotal which tracks
     // the recently rendered tables posts number, is less than
     // totalBlogPosts on the database.json(get that data again).
-    // IF it is, cycle through the difference and render those posts. 
+    // IF it is, cycle through the difference 
+    // i.e. slice the array ...  
+    // and render those posts. 
 
 });
