c6b10024355f923fe3be6286339f83bc01923072
Pallandor <roger.sejas@gmail.com>
2016-03-12 22:38:52 +1100

    Testing to see if cross-ss origin error with AJAX call will happen on website

--- blogapp.js
+++ blogapp.js
@@ -1,6 +1,18 @@
+// $ document.ready
+$(function() {
 
-// Do I need to specify full url? 
-$.get('/database.json', function(data){
-	//Testing grabbing the JSON data from the local database.json file. 
-	console.log(data); 
+    $.get('/database.json', function(data) {
+
+        // If jQuery doesn't automatically return a JS object, parse the JSON string
+        var blogData = typeof data === 'string' ? JSON.parse(data) : data;
+
+        var blogNumber = blogData.nextBlogNumber;
+        // THEN update nextBlogNumber in database.json! 
+
+        // INVESTIGATE: Properly using node_modules (require lib?)
+        var blogDate = moment().format('MMMM Do YYYY, h:mm:ss');
+
+        $('.blog-table-body').append('<tr><th>' + blogNumber + '</th><td>' + blogDate + '</td><td>Loading...</td></tr>')
+
+    });
 });
