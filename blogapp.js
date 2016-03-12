42b0d435deff6f9826b46c4e8d72915c0600ab86
Pallandor <roger.sejas@gmail.com>
2016-03-12 23:01:37 +1100

    Testing if my table rendering works using JSON data

--- blogapp.js
+++ blogapp.js
@@ -1,18 +1,34 @@
+// For purposes of adding a blog post: 
+// var localPostsTotal;
+// var renderTable = function(blog) {
+//     blog.blogPosts.forEach(function(post) {
+//         $('.blog-table-body').append('<tr><th>' + post.number + '</th><td>' + post.date + '</td><td>' + post.content + '</td></tr>');
+//     });
+// };
+
 // $ document.ready
 $(function() {
 
+    // Render blog table with current blog posts
     $.get('/database.json', function(data) {
 
         // If jQuery doesn't automatically return a JS object, parse the JSON string
         var blogData = typeof data === 'string' ? JSON.parse(data) : data;
+        localPostsTotal = blogData.totalBlogPosts;
 
-        var blogNumber = blogData.nextBlogNumber;
-        // THEN update nextBlogNumber in database.json! 
-
-        // INVESTIGATE: Properly using node_modules (require lib?)
-        var blogDate = moment().format('MMMM Do YYYY, h:mm:ss');
-
-        $('.blog-table-body').append('<tr><th>' + blogNumber + '</th><td>' + blogDate + '</td><td>Loading...</td></tr>')
-
+        blogData.blogPosts.forEach(function(post) {
+            $('.blog-table-body').append('<tr><th>' + post.number + '</th><td>' + post.date + '</td><td>' + post.content + '</td></tr>');
+        });
     });
+
+    // Add blog post
+    // 1. Update database.json
+    // 2. re - render the entire table ? yuck..OR
+    // manually add to table in this instance ? OR
+    // have blog continually check
+    // if localPostsTotal which tracks
+    // the recently rendered tables posts number, is less than
+    // totalBlogPosts on the database.json(get that data again).
+    // IF it is, cycle through the difference and render those posts. 
+
 });
