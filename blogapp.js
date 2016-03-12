00c56704d19a238a5cc2c0f0c7a5d6d4a3ff6b08
Pallandor <roger.sejas@gmail.com>
2016-03-13 00:58:31 +1100

    Testing Github API library for accessing repo by low level github api

--- blogapp.js
+++ blogapp.js
@@ -3,7 +3,7 @@ var table = {
     localPostsTotal: null,
     render: function tableRenderer(blog) {
         // Track the locally rendered table's total posts
-        
+
         // if table never rendered... localPosts=blogposts
         // but if table was rendered... then compare localPosts vs blogPosts
         // and the array to use will be a sliced vers of blogPosts
@@ -13,8 +13,9 @@ var table = {
         // like express app.post('url', function(){...THE STUFF TO DO...}); 
 
         // But its hosted on github. just do a git api call to update
-        // the database object like
+        // the database.json file like git add git push etc.. 
         // 
+
 
         blog.blogPosts.forEach(function(post) {
             $('.blog-table-body').append('<tr><th>' + post.number + '</th><td>' + post.date + '</td><td>' + post.content + '</td></tr>');
@@ -25,7 +26,7 @@ var table = {
 // $ document.ready
 $(function() {
 
-	// setInterval()
+    // setInterval()
 
     // Render blog table with current blog posts
     $.get('/database.json', function(data) {
@@ -45,5 +46,42 @@ $(function() {
     // IF it is, cycle through the difference 
     // i.e. slice the array ...  
     // and render those posts. 
+
+
+    // Let's test GIT api
+    // But if this code is not obfuscated, 
+    // when I do the authentication part... 
+    // it would need 
+    // I can just use limited scope personal access token. 
+    // $.ajax({
+    //     method: "PUT",
+    //     url: "https://api.github.com/repos/Pallandor/Pallandor.github.io/contents/test.json"
+    //     data: {
+    //         path: 'test.json',
+    //         message: 'Commit message for test GIT API update of test.json file',
+    //         content: window.btoa('{"did it replace":"we will find out"}'),
+    //         sha: '0dc205cbdaa5dcd142a43d8d4661bc534c98bad9'
+    //     }
+    // })
+
+    // USING: Github api library. 
+    // Have to reveal limited access token ugh. 
+    var github = new Github({
+    	token: '0dc205cbdaa5dcd142a43d8d4661bc534c98bad9',
+    	auth: 'oauth'
+    });
+
+    var repo = github.getRepo('Pallandor','Pallandor.github.io');
+
+    repo.getRef('heads/master', function(err, sha) {
+        console.log('the sha of my Pallandor.github.io repo is: ' + sha);
+    });
+
+    // var options = {
+    //     author: { name: 'Author Name', email: 'author@example.com' },
+    //     committer: { name: 'Committer Name', email: 'committer@example.com' },
+    //     encode: true // Whether to base64 encode the file. (default: true)
+    // }
+    // repo.write('master', 'path/to/file', 'YOUR_NEW_CONTENTS', 'YOUR_COMMIT_MESSAGE', options, function(err) {});
 
 });
