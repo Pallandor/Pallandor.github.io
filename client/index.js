425bea58914b089f2bda151f8f49f2b371df22b7
Pallandor <roger.sejas@gmail.com>
2016-09-02 17:29:51 +1000

    Use temporarily default hashHistory for react-router

--- client/index.js
+++ client/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router } from 'react-router';
-// import { Router, browserHistory } from 'react-router';
+import { Router } from 'react-router'; 
 
 import routes from './routes';
 import configureStore from './store/configureStore';
 
-const store = configureStore({'beginning': 'lololol'});
+const store = configureStore({});
 
 ReactDOM.render(
   <Provider store={ store }>
