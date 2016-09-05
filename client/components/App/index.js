8d202b18d9049d16cd2387036d3d29065d875476
Pallandor <roger.sejas@gmail.com>
2016-09-06 07:18:05 +1000

    (Checkpoint) Add CSS to style Components

--- client/components/App/index.js
+++ client/components/App/index.js
@@ -8,11 +8,27 @@ const App = ({ children }) => (
   <div>
     <NavBar />
     {children}
+    <div className="ui secondary pointing menu">
+      <a className="active item">
+        Home
+      </a>
+      <a className="item">
+        Messages
+      </a>
+      <a className="item">
+        Friends
+      </a>
+      <div className="right menu">
+        <a className="ui item">
+          Logout
+        </a>
+      </div>
+    </div>
   </div>
 );
 
 App.propTypes = {
-  children: React.PropTypes.node, 
+  children: React.PropTypes.node,
 };
 
 export default App;
