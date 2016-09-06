1bb9d2c76abe69229e72c8980e5b55ad4b7b0160
Pallandor <roger.sejas@gmail.com>
2016-09-06 11:33:26 +1000

    Add Tachyons CDN (temp) and style NavBar component

--- client/components/NavBar/index.js
+++ client/components/NavBar/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-// should props.children be <props.children /> ??
-
 const NavBar = () => (
   <div>
-    <nav>
-      <Link to={'/'}>Home</Link>
-      <Link to={'about'}>About</Link>
+    <nav className={styles.nav}>
+    <Link className={styles.leftLink} to={'/'}>Home</Link>
+      <div className={styles.rightContainer}>
+        <Link className={styles.rightLink} to={'about'}>About</Link>
+        <Link className={styles.rightLink} to={'about'}>Github</Link>
+        <Link className={styles.lastRightLink} to={'about'}>Contact</Link>
+      </div>
     </nav>
   </div>
 );
+
+// tachyon styles
+const styles = {
+  nav: 'dt w-100 border-box pa3 ph5-ns',
+  rightContainer: 'dtc v-mid w-75 tr',
+  leftLink: 'dtc v-mid link dim w-25 black b', 
+  rightLink: 'link dim dark-gray f6 f5-ns dib mr3 mr4-ns',
+  lastRightLink: 'link dim dark-gray f6 f5-ns dib',
+};
 
 export default NavBar;
