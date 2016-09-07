6ffe7a7b2359f79a68f66c8fd29a38b9fb8fa530
Pallandor <roger.sejas@gmail.com>
2016-09-07 16:37:17 +1000

    Fix date render in utils helper

--- client/util.js
+++ client/util.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 export const convertDateToString = dateObj => {
-  const mm = dateObj.getMonth() + 1;
-  const dd = dateObj.getDate();
-  // TODO: Fix so renders in format DD-MM-YYYY
-  return [dateObj.getFullYear(), !mm[1] && '0', mm, !dd[1] && '0', dd].join('');
+  let mm = dateObj.getMonth() + 1;
+  let dd = dateObj.getDate();
+  dd = [!dd[1] && '0', dd].join('');
+  mm = [!mm[1] && '0', mm].join('');
+  return [dd, mm, dateObj.getFullYear()].join('/');
 };
